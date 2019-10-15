cc3df549ba81547b5442938a14e37670f91545e5
Vishal Parpia <viz@activelement.com>
2019-10-15 21:23:39 +0800

    I think this will fix it

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, World!!', 
+        msg: 'Hello, World!', 
         environment: config.ENV_NAME
     }
 };
