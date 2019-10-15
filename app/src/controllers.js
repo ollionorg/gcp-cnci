2b44a3e8934e98f76d5950175ab1947dfb681905
Prashant <prashant@cldcvr.com>
2019-10-15 11:44:47 +0530

    Greetings for CldCvr

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'hello, World!', 
+        msg: 'hello, CldCvr!', 
         environment: config.ENV_NAME
     }
 };
