187e04927eaadeba525421b0cd57c1976e1dc19d
Vishal Parpia <viz@activelement.com>
2019-10-16 14:25:00 +0800

    Let's go native!

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, World!', 
+        msg: 'Hello, Singapore!!', 
         environment: config.ENV_NAME
     }
 };
