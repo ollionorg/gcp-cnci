73afff534b798705f304475c051991ad194650c6
Vishal Parpia <viz@activelement.com>
2019-10-16 14:27:08 +0800

    Ooopsie!

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, Singapore!!', 
+        msg: 'Hello, Singapore!', 
         environment: config.ENV_NAME
     }
 };
